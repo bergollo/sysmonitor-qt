d683dcbbcf534da50ae188e489cacf7e397e48a4
Bergollo <kevin.bergollo@gmail.com>
2026-07-25 21:24:25 +0000

    feat: add main window shell

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,15 @@
 #include <QApplication>
 #include <QLabel>
+#include <QMainWindow>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    QLabel helloWorld("Hello, World!");
-    helloWorld.resize(240, 80);
-    helloWorld.show();
+    QMainWindow mainWindow;
+    mainWindow.setCentralWidget(new QLabel("Placeholder"));
+    mainWindow.resize(640, 480);
+    mainWindow.show();
 
     return app.exec();
 }
